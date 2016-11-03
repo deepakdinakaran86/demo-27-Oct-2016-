369a697c24776fc2385a33df18db45a96d2cbdbf
Deepak Dinakaran <deepakdinakaran@Deepaks-MacBook-Pro.local>
2016-11-03 07:40:06 +0400

    Initial commit

--- src/main/java/com/arc/DemoApplication.java
+++ src/main/java/com/arc/DemoApplication.java
@@ -12,7 +12,7 @@ public class DemoApplication extends SpringBootServletInitializer {
 	
 	@RequestMapping("/")
 	public String home(){
-		return "Hello World";
+		return "Hello World from Deepak";
 	}
 
 	public static void main(String[] args) {
